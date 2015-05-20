b737f705f76043f3bb18fd87710f683d0ff81acb
Manh Tan Nguyen <manh_tan_nguyen@epam.com>
2015-05-21 01:26:10 +0300

    new Jboss script

--- setup_jboss.sh
+++ setup_jboss.sh
@@ -3,7 +3,7 @@
 # setup_jboss.sh
 #
 # Download and extract JBoss, config it to listen on all IP address and set Jboss to be daemon and auto start with machine
-
+CUR_DIR=$(pwd)
 DOWNLOAD_LINK="http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.tar.gz";
 TARGET_DIR="/opt";
 JBOSS_HOME="$TARGET_DIR/Jboss"
@@ -25,18 +25,9 @@ echo "Extracting $jboss_tar_file";
 tar xzvf $jboss_tar_file -C $JBOSS_HOME --strip=1
 chown -R vagrant:vagrant $JBOSS_HOME;
 
-# Modify standalone profile to listen on all IP addresses
-sed -iE "s/<inet-.*127.*/<any-ipv4-address\/>/" "$JBOSS_HOME/standalone/configuration/standalone.xml"
-
-# Change ajp port to 9091
-new_port=9091;
-sed -ie "s/\(.*ajp\" port=\"\)[0-9]*\(\"\/\)/\1$new_port\2/" "$JBOSS_HOME/standalone/configuration/standalone.xml"
-
 # change content of index in sample .war so we can know what server is this
 # Get current IP address of server
 SERVER_IP=`ifconfig eth1 | awk '/inet addr/{print substr($2,6)}'`;
-
-
 cd /tmp/
 wget --no-check-certificate https://tomcat.apache.org/tomcat-6.0-doc/appdev/sample/sample.war
 jar xvf sample.war
@@ -46,23 +37,12 @@ jar cvf sample.war *
 cp sample.war $JBOSS_HOME/standalone/deployments/
 cd $JBOSS_HOME
 
-# Make JBoss a Daemon and set it to automatically startic
-ln -s $JBOSS_HOME/bin/init.d/jboss-as-standalone.sh /etc/init.d/jboss
-chmod +x /etc/init.d/jboss
-# chkconfig --level 356 jboss on
+# Copy clusterwebapp to deployments
+cp $CUR_DIR/ClusterWebApp.war $JBOSS_HOME/standalone/deployments/
 
-# Configuration dir for JBOSS
-mkdir /etc/jboss-as
-echo -e "JBOSS_HOME=$JBOSS_HOME" > /etc/jboss-as/jboss-as.conf
-echo "JBOSS_CONSOLE_LOG=/var/log/jboss-console.log" >> /etc/jboss-as/jboss-as.conf
-echo "JBOSS_USER=root" >> /etc/jboss-as/jboss-as.conf
+last_ip=`echo "${SERVER_IP: -2}"`
+node_name="node$last_ip"
 
-# Download a simple Helloworld to deply to Jboss
-cd "$JBOSS_HOME/standalone/deployments/";
-
-# Download ClusterWebApp
-wget http://github.com/jaysensharma/MiddlewareMagicDemos/blob/master/ClusterTest_WebApp/ClusterWebApp.war
-chown vagrant:vagrant *
-
+CLUTER_BROADCAST="203.0.0.4"
 # Start JBoss
-#service jboss start
+$JBOSS_HOME/bin/standalone.sh -c standalone-ha.xml -b 0.0.0.0 -c $CLUSTER_BROADCAST --Djboss.node.name=$node_name
