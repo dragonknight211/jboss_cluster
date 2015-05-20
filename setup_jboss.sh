f7d5c40b74151037fbb05d85964e21ff42f6d651
Manh Tan Nguyen <manh_tan_nguyen@epam.com>
2015-05-21 01:41:36 +0300

    New Jboss scriptw

--- setup_jboss.sh
+++ setup_jboss.sh
@@ -40,7 +40,7 @@ cd $JBOSS_HOME
 # Copy clusterwebapp to deployments
 cp $CUR_DIR/ClusterWebApp.war $JBOSS_HOME/standalone/deployments/
 
-chown -R vagrant:vagrant $JBOSS_HOME/standalone/deployments/*
+chown -R vagrant:vagrant $JBOSS_HOME
 
 last_ip=`echo "${SERVER_IP: -2}"`
 node_name="node$last_ip"
